f8b862966b37080aa494858ad544d9df4a5d2caa
Michael Krishna <michaelkrishna@hotmail.com>
2020-11-26 20:23:42 +0000

    Refatctor getAllProperties to allow searching

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -87,12 +87,54 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} 
+    `;
+  }
+  if (options.owner_id) {
+    queryString += queryParams.length >= 1 ? `AND ` : `WHERE `;
+    queryParams.push(options.owner_id);
+    queryString += `owner_id = $${queryParams.length}
+    `;
+  }
+  if (options.minimum_price_per_night) {
+    queryString += queryParams.length >= 1 ? `AND ` : `WHERE `;
+    queryParams.push(options.minimum_price_per_night);
+    queryString += `cost_per_night >= $${queryParams.length}
+    `;
+  }
+  if (options.maximum_price_per_night) {
+    queryString += queryParams.length >= 1 ? `AND ` : `WHERE `;
+    queryParams.push(options.maximum_price_per_night);
+    queryString += `cost_per_night <= $${queryParams.length}
+    `;
+  }
+
+  
+  queryString += `GROUP BY properties.id
+  `;
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length}
+    `;
+  }
+  queryParams.push(limit);
+  queryString += `
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+  `;
+  return pool.query(queryString, queryParams)
   .then(res => res.rows);
 }
+
 
 
 exports.getAllProperties = getAllProperties;
